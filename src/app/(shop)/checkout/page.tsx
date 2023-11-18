5b363ccb7bba26496f16a16ab327fcd8814c3ec1
devlopersabbir <devlopersabbir@gmail.com>
2023-11-18 20:50:38 +0600

    checkout | customer | settings page added

---
+++ src/app/(shop)/checkout/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>check out page</div>;
+};
+
+export default page;
---
+++ src/app/admin/customers/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>customer</div>;
+};
+
+export default page;
