a047ceec62487a9128ea7372077254fe00670c7d
devlopersabbir <devlopersabbir@gmail.com>
2023-11-18 21:58:01 +0600

    Customer data shows perfectly

--- src/app/(shop)/checkout/page.tsx
+++ src/app/(shop)/checkout/page.tsx
@@ -1,7 +1,37 @@
+import PageHeading from "@/app/admin/_components/page-heading";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import React from "react";
 
-const page = () => {
-  return <div>check out page</div>;
+const Checkout = () => {
+  return (
+    <div>
+      <PageHeading title="Checkout" />
+      <div className="flex justify-between w-full border-2 my-5">
+        <div className="leftSide  w-[65%] p-4">
+          <h1 className="font-semibold">Shipping Details</h1>
+          <form>
+            <label htmlFor="address">Address</label>
+            <Input type="text" placeholder="Enter your address" />
+          </form>
+        </div>
+        <div className="rightSide  w-[35%] dark:bg-zinc-800 bg-gray-300 p-4 rounded-md shadow-lg dark:text-white text-gray-900 flex flex-col gap-5">
+          <h1 className="text-2xl font-semibold">Your Order</h1>
+          <div className="details border-b-2">
+            <p>Order details/address</p>
+            <p>Order details/address</p>
+            <p>Order details/address</p>
+            <p>Order details/address</p>
+          </div>
+          <div className="amount border-b-2">
+            <h1>Total Ammount</h1>
+            <p className="font-semibold text-xl">$223</p>
+          </div>
+          <Button className="w-full">Place Order</Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default Checkout;
---
+++ src/app/admin/customers/_components/customer-table.tsx
@@ -0,0 +1,64 @@
+import prisma from "@/lib/db";
+import {
+  Table,
+  TableCaption,
+  TableHeader,
+  TableRow,
+  TableHead,
+  TableBody,
+  TableCell,
+} from "@/components/ui/table";
+import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+export default async function CustomerTable() {
+  const customers = await prisma.user.findMany();
+  console.log("customer", customers);
+
+  return (
+    <Table>
+      <TableCaption>A list of your Customer</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">ID</TableHead>
+          <TableHead>Name</TableHead>
+          <TableHead>Role</TableHead>
+          <TableHead>Email</TableHead>
+          <TableHead className="text-right">Image</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {customers.length < 1 ? (
+          <p>No Cusomer in database</p>
+        ) : (
+          customers.map((customer) => (
+            <TableRow key={customer.id}>
+              <TableCell className="font-medium">{customer.id}</TableCell>
+              <TableCell>
+                <Link href={`/admin/customers/edit/${customer.name}`}>
+                  {customer.name}
+                </Link>
+              </TableCell>
+              <TableCell className="font-medium">{customer.role}</TableCell>
+              <TableCell className="font-medium">{customer.email}</TableCell>
+              <TableCell className="text-right">
+                {customer.name ? (
+                  <Avatar className="h-8 w-8">
+                    {customer.image ? (
+                      <AvatarImage src={customer.image} alt={customer.name} />
+                    ) : null}
+                    <AvatarFallback>
+                      {customer.name
+                        .split(" ")
+                        .map((n, i) => (i < 2 ? n.split("")[0] : null))}
+                    </AvatarFallback>
+                  </Avatar>
+                ) : null}
+              </TableCell>
+            </TableRow>
+          ))
+        )}
+      </TableBody>
+    </Table>
+  );
+}
